a03b2319a8e38fdaf5567d8931b16ed1fdd1cfb0
Illia kalinin <illinoiz4305@gmail.com>
2022-10-31 22:05:08 +0200

    Add js basic homework

---
+++ js/script.js
@@ -0,0 +1,41 @@
+// const a = 2;
+// const b = 3;
+// console.log(" a * b = ", a * b);
+
+// const c = 10;
+// const d = 2;
+// console.log(" c / d = ", c / d);
+
+// const e = 2;
+// const f = 3;
+// console.log(" e + f = ", e + f);
+
+// console.log("typeof 11 :>> ", typeof 11);
+// console.log("typeof true :>> ", typeof true);
+// console.log('typeof "java script" :>> ', typeof "java script");
+// console.log('typeof "100" :>> ', typeof "100");
+
+// let num = 1;
+// num += 11;
+// num -= 11;
+// num *= 11;
+// num /= 11;
+// num++;
+// num--;
+
+// let a = Number(prompt("Введіть число"));
+// let b = a * a;
+// alert("Результат = " + b);
+
+// let number1 = Number(prompt("Введіть перше число"));
+// let number2 = Number(prompt("Введіть друге число"));
+// let result = (number1 + number2) / 2;
+// alert("Середнє арифметичне = " + result);
+
+// const minutes = Number(prompt("Введіть час"));
+// const seconds = minutes * 60;
+// alert("Кількість секунд в даному часі = " + seconds);
+
+const greeting = "Hello";
+var username = prompt("Введіть ваше ім'я");
+alert("Hello, " + username + "!");
